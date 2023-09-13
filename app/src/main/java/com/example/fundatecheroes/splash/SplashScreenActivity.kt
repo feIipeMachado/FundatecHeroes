e0e35f7688b0491e5a6ef013ba15c28eb1730baf
felipe machado <machadofelipe677@gmail.com>
2023-09-13 15:58:15 -0300

    tela de login e tela de splash

---
+++ app/src/main/java/com/example/fundatecheroes/splash/SplashScreenActivity.kt
@@ -0,0 +1,18 @@
+package com.example.fundatecheroes.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fundatecheroes.HomeActivity
+import com.example.fundatecheroes.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+
+
+
+
+}
