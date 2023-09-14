464e3e72984dc3806c800c90f198d27675ebde9e
felipe machado <machadofelipe677@gmail.com>
2023-09-13 22:15:50 -0300

    navegaçao entre tela de splash, tela de login e tela home

--- app/src/main/java/com/example/fundatecheroes/login/LoginScreenActivity.kt
+++ app/src/main/java/com/example/fundatecheroes/login/LoginScreenActivity.kt
@@ -1,12 +1,26 @@
 package com.example.fundatecheroes.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.fundatecheroes.HomeActivity
 import com.example.fundatecheroes.R
+import com.example.fundatecheroes.home.presentation.model.HomeViewState
 
 class LoginScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
+
+        val login = findViewById<Button>(R.id.login)
+
+        login.setOnClickListener {
+            val intent = Intent(this@LoginScreenActivity, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
+
 }
